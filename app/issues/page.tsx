4d6e97482926a6180ba9d9f81ca499b104ba9773
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-02-29 11:45:28 +0300

    add: radix-ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from '@radix-ui/themes';
 import React from 'react';
 
 export default function IssuesPage() {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 }
