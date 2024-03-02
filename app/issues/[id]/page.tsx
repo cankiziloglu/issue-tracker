222728a4ceee964052100d924c3cef34338da8e0
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-03-02 13:40:21 +0300

    add edit button to issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import prisma from '@/prisma/client';
 import { notFound } from 'next/navigation';
-import { Card, Flex, Heading } from '@radix-ui/themes';
+import { Box, Button, Card, Flex, Grid, Heading } from '@radix-ui/themes';
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import Markdown from 'react-markdown';
+import Link from 'next/link';
+import { AiFillEdit } from 'react-icons/ai';
 
 export default async function IssueDetailPage({
   params,
@@ -17,15 +19,23 @@ export default async function IssueDetailPage({
   if (!issue) notFound();
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap='4' my='4'>
-        <IssueStatusBadge status={issue.status} />
-        <p>{issue.createdAt.toDateString()}</p>
-      </Flex>
-      <Card className='prose'>
-        <Markdown>{issue.description}</Markdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: '1', sm: '2' }} gap='5'>
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap='4' my='4'>
+          <IssueStatusBadge status={issue.status} />
+          <p>{issue.createdAt.toDateString()}</p>
+        </Flex>
+        <Card className='prose'>
+          <Markdown>{issue.description}</Markdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <AiFillEdit />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 }
