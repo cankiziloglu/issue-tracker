ac32ab602f3dc0a94b262ab7cc84afdb2068b89c
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-03-02 14:55:07 +0300

    add delete button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,7 +5,7 @@ import { Box, Button, Card, Flex, Grid, Heading } from '@radix-ui/themes';
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import Markdown from 'react-markdown';
 import Link from 'next/link';
-import { AiFillEdit } from 'react-icons/ai';
+import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 
 export default async function IssueDetailPage({
   params,
@@ -19,22 +19,28 @@ export default async function IssueDetailPage({
   if (!issue) notFound();
 
   return (
-    <Grid columns={{ initial: '1', sm: '2' }} gap='5'>
-      <Box>
+    <Grid columns={{ initial: '1', sm: '5' }} gap='5'>
+      <Box className='md:col-span-4'>
         <Heading>{issue.title}</Heading>
         <Flex gap='4' my='4'>
           <IssueStatusBadge status={issue.status} />
           <p>{issue.createdAt.toDateString()}</p>
         </Flex>
-        <Card className='prose'>
+        <Card className='prose max-w-full'>
           <Markdown>{issue.description}</Markdown>
         </Card>
       </Box>
       <Box>
-        <Button>
-          <AiFillEdit />
-          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <Flex direction='column' gap='4'>
+          <Button>
+            <AiFillEdit />
+            <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+          </Button>
+          <Button color='red'>
+            <AiFillDelete />
+            <Link href={`/issues/${issue.id}/delete`}>Delete Issue</Link>
+          </Button>
+        </Flex>
       </Box>
     </Grid>
   );
