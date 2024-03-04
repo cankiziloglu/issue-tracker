885b7c0f1d7d5ed66b8022c7e5fade12562c5a47
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-03-04 15:02:25 +0300

    refactor: navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -8,6 +8,57 @@ import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
 
 export default function NavBar() {
+  return (
+    <nav className='p-5 border-b mb-5'>
+      <Container size='3'>
+        <Flex gap='4' align='center' justify='between'>
+          <Flex gap='4' align='center'>
+            <Logo />
+            <NavLinks />
+          </Flex>
+          <AuthState />
+        </Flex>
+      </Container>
+    </nav>
+  );
+}
+
+function AuthState() {
+  const session = useSession();
+
+  if (session.status === 'loading') return null;
+  if (session.status === 'unauthenticated')
+    return (
+      <Link href='/api/auth/signin' className='nav-link'>
+        Sign in
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session.data?.user?.image!}
+            fallback='?'
+            radius='full'
+            size='2'
+            referrerPolicy='no-referrer'
+            className='cursor-pointer'
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content align='end'>
+          <DropdownMenu.Label>{session.data?.user?.email!}</DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href='/api/auth/signout'>Sign out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+}
+
+function NavLinks() {
   const links = [
     {
       title: 'Dashboard',
@@ -21,62 +72,28 @@ export default function NavBar() {
 
   const activeLink = usePathname();
 
-  const session = useSession();
-
   return (
-    <nav className='p-5 border-b mb-5'>
-      <Container size='3'>
-        <Flex gap='4' align='center' justify='between'>
-          <Flex gap='4' align='center'>
-            <Link href='/'>
-              <AiFillBug className='text-2xl' />
-            </Link>
-            <ul className='flex space-x-6'>
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    href={link.href}
-                    className={`${
-                      link.href === activeLink
-                        ? 'text-zinc-900'
-                        : 'text-zinc-500'
-                    } hover:text-zinc-800 transition-all`}
-                  >
-                    {link.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          <Box>
-            {session.status === 'authenticated' && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.data?.user?.image!}
-                    fallback='?'
-                    radius='full'
-                    size='2'
-                    referrerPolicy='no-referrer'
-                    className='cursor-pointer'
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content align='end'>
-                  <DropdownMenu.Label>
-                    {session.data?.user?.email!}
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href='/api/auth/signout'>Sign out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {session.status === 'unauthenticated' && (
-              <Link href='/api/auth/signin'>Sign in</Link>
-            )}
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+    <ul className='flex space-x-6'>
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            className={`${
+              link.href === activeLink ? 'text-zinc-900' : 'nav-link'
+            }`}
+          >
+            {link.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Logo() {
+  return (
+    <Link href='/'>
+      <AiFillBug className='text-2xl' />
+    </Link>
   );
 }
